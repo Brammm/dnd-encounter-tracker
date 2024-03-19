1949370fc3d7114f8b998ab97975cd6f767bbd01
Bram Van der Sype <bram.vandersype@gmail.com>
2024-03-19 16:23:01 +0100

    Show history

--- src/components/CharacterRow.tsx
+++ src/components/CharacterRow.tsx
@@ -16,6 +16,7 @@ export function CharacterRow({character, encounterId}: Props) {
         hpChanges: encounters[encounterId].hpChanges.filter((hpChange) => hpChange.characterId === character.id),
     }));
     const [amount, setAmount] = useState<string>('');
+    const [showHistory, setShowHistory] = useState<boolean>(false);
 
     const handleModify = (multiplier: number = 1) => {
         modifyHp(encounterId, character.id, parseInt(amount) * multiplier);
@@ -29,65 +30,97 @@ export function CharacterRow({character, encounterId}: Props) {
     }
 
     return (
-        <div
-            className={clsx(
-                'flex border rounded-lg mb-2',
-                character.takingTurn ? 'border-cyan-400' : 'border-gray-200',
-            )}
-        >
-            <div className={clsx('flex flex-col p-2 rounded-l', character.takingTurn ? 'bg-cyan-400' : 'bg-gray-200')}>
-                <span>Initiative</span>
-                <EditableText
-                    value={character.initiative.toString()}
-                    onChange={(value) => updateInitiative(encounterId, character.id, parseInt(value))}
-                />
-            </div>
-            <div className="place-self-center px-4 flex">
-                {character.type === 'PC' && <Icon className="h-4 mr-2 text-gray-400 place-self-center" type="player" />}
-                {character.type === 'NPC' && <Icon className="h-4 mr-2 text-gray-400 place-self-center" type="skull" />}
-                {character.name}
-            </div>
-            {character.hp !== undefined && (
-                <div className="flex border-l p-2 place-items-center">
-                    <div className="relative">
-                        <span>
-                            <abbr title={character.hp.toString()}>{currentHp}</abbr> HP
-                        </span>
-                        {hpPercentage !== undefined && (
-                            <div
-                                className={clsx(
-                                    'absolute bottom-0 w-full rounded-full h-1.5',
-                                    hpPercentage === 0 ? 'bg-black' : 'bg-gray-500',
-                                )}
-                            >
+        <div className="mb-2">
+            <div
+                className={clsx(
+                    'flex border',
+                    showHistory ? 'rounded-t-lg' : 'rounded-lg',
+                    character.takingTurn ? 'border-cyan-400' : 'border-gray-200',
+                )}
+            >
+                <div
+                    className={clsx(
+                        'flex flex-col p-2',
+                        showHistory ? 'rounded-tl-lg' : 'rounded-l-lg',
+                        character.takingTurn ? 'border-cyan-400' : 'border-gray-200',
+                        character.takingTurn ? 'bg-cyan-400' : 'bg-gray-200',
+                    )}
+                >
+                    <span>Initiative</span>
+                    <EditableText
+                        value={character.initiative.toString()}
+                        onChange={(value) => updateInitiative(encounterId, character.id, parseInt(value))}
+                    />
+                </div>
+                <div className="place-self-center px-4 flex">
+                    {character.type === 'PC' && (
+                        <Icon className="h-4 mr-2 text-gray-400 place-self-center" type="player" />
+                    )}
+                    {character.type === 'NPC' && (
+                        <Icon className="h-4 mr-2 text-gray-400 place-self-center" type="skull" />
+                    )}
+                    {character.name}
+                </div>
+                {character.hp !== undefined && (
+                    <div className="flex border-l p-2 place-items-center">
+                        <div className="relative">
+                            <span>
+                                <abbr title={character.hp.toString()}>{currentHp}</abbr> HP
+                            </span>
+                            {hpPercentage !== undefined && (
                                 <div
                                     className={clsx(
-                                        'h-1.5 rounded-full',
-                                        hpPercentage >= 60
-                                            ? 'bg-green-600'
-                                            : hpPercentage >= 30
-                                            ? 'bg-orange-400'
-                                            : 'bg-red-500',
+                                        'absolute bottom-0 w-full rounded-full h-1.5',
+                                        hpPercentage === 0 ? 'bg-black' : 'bg-gray-500',
                                     )}
-                                    style={{width: `${hpPercentage}%`}}
-                                ></div>
-                            </div>
-                        )}
+                                >
+                                    <div
+                                        className={clsx(
+                                            'h-1.5 rounded-full',
+                                            hpPercentage >= 60
+                                                ? 'bg-green-600'
+                                                : hpPercentage >= 30
+                                                ? 'bg-orange-400'
+                                                : 'bg-red-500',
+                                        )}
+                                        style={{width: `${hpPercentage}%`}}
+                                    ></div>
+                                </div>
+                            )}
+                        </div>
+                        <div>
+                            <input
+                                type="number"
+                                name={`character-hp-${character.id}`}
+                                value={amount}
+                                onChange={(e) => setAmount(e.currentTarget.value)}
+                            />
+                            <button type="button" onClick={() => handleModify()}>
+                                Heal
+                            </button>
+                            <button type="button" onClick={() => handleModify(-1)}>
+                                Damage
+                            </button>
+                        </div>
                     </div>
-                    <div>
-                        <input
-                            type="number"
-                            name={`character-hp-${character.id}`}
-                            value={amount}
-                            onChange={(e) => setAmount(e.currentTarget.value)}
-                        />
-                        <button type="button" onClick={() => handleModify()}>
-                            Heal
-                        </button>
-                        <button type="button" onClick={() => handleModify(-1)}>
-                            Damage
+                )}
+                {hpChanges.length > 0 && (
+                    <div className="flex ml-auto place-items-center border-l px-2">
+                        <button onClick={() => setShowHistory(!showHistory)}>
+                            <Icon type={showHistory ? 'chevronup' : 'chevrondown'} className="h-4" />
                         </button>
                     </div>
+                )}
+            </div>
+            {showHistory && (
+                <div className="rounded-b-lg border-b border-x">
+                    <ul>
+                        {hpChanges.map((change, i) => (
+                            <li key={i}>
+                                {change.amount} &rarr; {change.changedHp}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             )}
         </div>
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,35 +1,53 @@
-import {PropsWithChildren} from 'react';
-
 type Props = {
-    type: 'skull' | 'player';
+    type: 'skull' | 'player' | 'chevrondown' | 'chevronup';
     className?: string;
 };
 
 export default function Icon({className, type}: Props) {
-    const Svg = ({children, viewBox}: PropsWithChildren<{viewBox: string}>) => (
-        <svg className={className} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
-            {children}
-        </svg>
-    );
-
     switch (type) {
         case 'player':
             return (
-                <Svg viewBox="0 0 448 512">
+                <svg className={className} viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"
                         fill="currentColor"
                     />
-                </Svg>
+                </svg>
             );
         case 'skull':
             return (
-                <Svg viewBox="0 0 512 512">
+                <svg className={className} viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M416 398.9c58.5-41.1 96-104.1 96-174.9C512 100.3 397.4 0 256 0S0 100.3 0 224c0 70.7 37.5 133.8 96 174.9c0 .4 0 .7 0 1.1v64c0 26.5 21.5 48 48 48h48V464c0-8.8 7.2-16 16-16s16 7.2 16 16v48h64V464c0-8.8 7.2-16 16-16s16 7.2 16 16v48h48c26.5 0 48-21.5 48-48V400c0-.4 0-.7 0-1.1zM96 256a64 64 0 1 1 128 0A64 64 0 1 1 96 256zm256-64a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"
                         fill="currentColor"
                     />
-                </Svg>
+                </svg>
+            );
+        case 'chevrondown':
+            return (
+                <svg
+                    className={className}
+                    fill="none"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="m19.5 8.25-7.5 7.5-7.5-7.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                </svg>
+            );
+        case 'chevronup':
+            return (
+                <svg
+                    className={className}
+                    fill="none"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="m4.5 15.75 7.5-7.5 7.5 7.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                </svg>
             );
     }
 }
