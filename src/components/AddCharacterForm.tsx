9ab6ac54b8e8d7a4764c7d151600677a0f5ca51b
Bram Van der Sype <bram.vandersype@gmail.com>
2024-04-09 14:44:48 +0200

    Add hp multiplier

--- src/components/AddCharacterForm.tsx
+++ src/components/AddCharacterForm.tsx
@@ -5,17 +5,25 @@ import InputGroup from './InputGroup.tsx';
 import InitiativeInput from './InitiativeInput.tsx';
 import HpInput from './HpInput.tsx';
 import NameComboBox, {CharacterData} from './NameComboBox.tsx';
+import Input from './Input.tsx';
+
+type CharacterType = 'PC' | 'NPC';
 
 type Props = {
-    onAdd: (type: 'PC' | 'NPC', name: string, initiative: number, hp?: number) => void;
+    onAdd: (type: CharacterType, name: string, initiative: number, hp?: number) => void;
 };
 
+type FormState = {type: CharacterType; name: string; hp: string; initiative: string; settings: {multiplier: number}};
+
 export default function AddCharacterForm({onAdd}: Props) {
-    const [state, setState] = useState<{type: 'PC' | 'NPC'; name: string; hp: string; initiative: string}>({
+    const [state, setState] = useState<FormState>({
         type: 'NPC',
         name: '',
         hp: '',
         initiative: '',
+        settings: {
+            multiplier: 1,
+        },
     });
 
     function handleSubmit(e: FormEvent) {
@@ -43,7 +51,7 @@ export default function AddCharacterForm({onAdd}: Props) {
         setState((prevState) => {
             prevState.name = data.name;
             if (!data.custom) {
-                prevState.hp = data.hp.average;
+                prevState.hp = Math.floor(parseInt(data.hp.average) * state.settings.multiplier).toString();
             }
 
             return {...prevState};
@@ -51,46 +59,64 @@ export default function AddCharacterForm({onAdd}: Props) {
     }
 
     return (
-        <form onSubmit={handleSubmit} className={'flex gap-x-4 my-6'}>
-            <p className="flex flex-col justify-end">
-                <RadioButton
-                    checked={state.type === 'NPC'}
-                    id="type-NPC"
-                    label="NPC"
-                    name="type"
-                    value="NPC"
-                    onChange={(value) => setState((prevState) => ({...prevState, type: value as 'PC' | 'NPC'}))}
-                />
-                <RadioButton
-                    checked={state.type === 'PC'}
-                    id="type-PC"
-                    label="PC"
-                    name="type"
-                    value="PC"
-                    onChange={(value) => setState((prevState) => ({...prevState, type: value as 'PC' | 'NPC'}))}
-                />
-            </p>
-            <InputGroup id="name" label="Name">
-                <NameComboBox
-                    onChange={handleCharacterSelect}
-                    value={{name: state.name, hp: {average: '', formula: ''}, custom: false}}
-                />
-            </InputGroup>
-            <InputGroup id="hp" label="HP">
-                <HpInput
-                    name="hp"
-                    value={state.hp}
-                    onChange={(value) => setState((prevState) => ({...prevState, hp: value}))}
-                />
-            </InputGroup>
-            <InputGroup id="initiative" label="Initiative">
-                <InitiativeInput
-                    name="initiative"
-                    onChange={(value) => setState((prevState) => ({...prevState, initiative: value}))}
-                />
-            </InputGroup>
-            <div className="flex flex-col justify-end">
-                <Button submit>Add</Button>
+        <form onSubmit={handleSubmit}>
+            <div className={'flex gap-x-4 my-6'}>
+                <p className="flex flex-col justify-end">
+                    <RadioButton
+                        checked={state.type === 'NPC'}
+                        id="type-NPC"
+                        label="NPC"
+                        name="type"
+                        value="NPC"
+                        onChange={(value) => setState((prevState) => ({...prevState, type: value as 'PC' | 'NPC'}))}
+                    />
+                    <RadioButton
+                        checked={state.type === 'PC'}
+                        id="type-PC"
+                        label="PC"
+                        name="type"
+                        value="PC"
+                        onChange={(value) => setState((prevState) => ({...prevState, type: value as 'PC' | 'NPC'}))}
+                    />
+                </p>
+                <InputGroup id="name" label="Name">
+                    <NameComboBox
+                        onChange={handleCharacterSelect}
+                        value={{name: state.name, hp: {average: '', formula: ''}, custom: false}}
+                    />
+                </InputGroup>
+                <InputGroup id="hp" label="HP">
+                    <HpInput
+                        name="hp"
+                        value={state.hp}
+                        onChange={(value) => setState((prevState) => ({...prevState, hp: value}))}
+                    />
+                </InputGroup>
+                <InputGroup id="initiative" label="Initiative">
+                    <InitiativeInput
+                        name="initiative"
+                        onChange={(value) => setState((prevState) => ({...prevState, initiative: value}))}
+                    />
+                </InputGroup>
+                <div className="flex flex-col justify-end">
+                    <Button submit>Add</Button>
+                </div>
+            </div>
+            <div>
+                <InputGroup id="multiplier" label="Multiplier">
+                    <Input
+                        type="number"
+                        name="multiplier"
+                        onChange={(value) =>
+                            setState((prevState) => ({
+                                ...prevState,
+                                settings: {...prevState.settings, multiplier: parseFloat(value)},
+                            }))
+                        }
+                        step={0.1}
+                        value={state.settings.multiplier.toString()}
+                    />
+                </InputGroup>
             </div>
         </form>
     );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import {clsx} from 'clsx';
 
 export type InputProps = {
     id?: string;
     name: string;
     type?: 'text' | 'number';
+    value?: string;
+    onChange?: (value: string) => void;
+    step?: number;
 };
 
 export const defaultClassName = 'border-gray-500 rounded';
 
-export default function Input({id, name, type = 'text'}: InputProps) {
-    const extraProps: React.InputHTMLAttributes<HTMLInputElement> = type === 'number' ? {step: 1, min: 0} : {};
+export default function Input({id, name, onChange, step = 1, type = 'text', value}: InputProps) {
+    const extraProps: React.InputHTMLAttributes<HTMLInputElement> = type === 'number' ? {step, min: 0} : {};
 
-    return <input id={id} name={name} type={type} className={clsx(defaultClassName, 'w-56')} {...extraProps} />;
+    const handleChange = onChange ? (e: ChangeEvent<HTMLInputElement>) => onChange(e.currentTarget.value) : undefined;
+
+    return (
+        <input
+            id={id}
+            name={name}
+            type={type}
+            className={clsx(defaultClassName, 'w-56')}
+            value={value}
+            onChange={handleChange}
+            {...extraProps}
+        />
+    );
 }
